3a447c1398f37f3ba12bbbd8e38fbe9bd35c6f6c
marek <asiaa75@o2.pl>
2026-05-11 16:38:48 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <nav>
       <header>
       Homepage
         <a
@@ -11,10 +11,26 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <ul>
+            <li><Link to="/">Home</Link></li>
+              <li><Link to="/">About</Link></li>
+              <li><Link to="/">Menu</Link></li>
+              <li><Link to="/">Reservation</Link></li>
+              <li><Link to="/">Order online</Link></li>
+              <li><Link to="/">Login</Link></li>
+          </ul>
           Learn React
         </a>
       </header>
-    </div>
+       <main className="Home">
+        <h1>Little Lemon</h1>
+        <p>About us</p>
+        <button>Little Lemon</button>
+      </main>
+      <footer className="site-footer">
+        <p>2026 Little Lemon.</p>
+      </footer>
+    </nav>
   );
 }
 
